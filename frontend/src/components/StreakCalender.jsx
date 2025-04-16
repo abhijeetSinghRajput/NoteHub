5958868e914b5199d3a613c8f938ce808a141c0e
Abhijeet Singh <abhijeet62008@gmail.com>
2025-04-16 12:52:10 +0530

    feat: refactor StreakCalender component for improved readability and structure

--- frontend/src/components/StreakCalender.jsx
+++ frontend/src/components/StreakCalender.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import TooltipWrapper from "./TooltipWrapper";
 
-const months = [
+const MONTHS = [
   "Jan",
   "Feb",
   "Mar",
@@ -16,7 +17,7 @@ const months = [
   "Dec",
 ];
 
-const Days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const StreakCalender = () => {
   return (
@@ -24,25 +25,37 @@ const StreakCalender = () => {
       <CardHeader>
         <CardTitle>346 Contribution in last year</CardTitle>
       </CardHeader>
-      <CardContent className="">
-        <div className="flex items-center gap-12">
-          {months.map((month) => (
-            <span>{month}</span>
-          ))}
-        </div>
-        <div className="flex space-x-1">
-          {Array(52)
-            .fill(null)
-            .map(() => (
-              <div className="week space-y-1">
-                {Array(7)
-                  .fill(null)
-                  .map((_, index) => (
-                    <div className="weekday flex items-center justify-center size-[14px] text-sm rounded bg-secondary">
-                    </div>
-                  ))}
+      <CardContent className="pb-2 w-full">
+        <div className="overflow-x-scroll flex gap-2 pb-2">
+          <div className="space-y-1">
+            {DAYS.map((day, i) => (
+              <div className="mt-4 weekday flex items-center justify-start h-[14px] text-xs font-semibold rounded">
+                {day.slice(0, 3)}
               </div>
             ))}
+          </div>
+          <div>
+            <div className="flex text-xs font-semibold items-center gap-12">
+              {MONTHS.map((month) => (
+                <span>{month}</span>
+              ))}
+            </div>
+            <div className="flex space-x-1">
+              {Array(52)
+                .fill(null)
+                .map((_, weekIndex) => (
+                  <div className="week space-y-1">
+                    {Array(7)
+                      .fill(null)
+                      .map((_, dayIndex) => (
+                        <TooltipWrapper message={`week ${weekIndex} : day ${dayIndex}`}>
+                          <div className="weekday flex items-center justify-center size-[14px] text-sm rounded bg-secondary"></div>
+                        </TooltipWrapper>
+                      ))}
+                  </div>
+                ))}
+            </div>
+          </div>
         </div>
       </CardContent>
     </Card>
